6b5034955e20502b2b962db217a3d11091ee2c27
Code Nation 20 <codenation@CN-20.lan>
2019-02-06 17:12:07 +0000

    working kit no styling

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
 // Play drum by keyboard press
 
+let play = () => {
+    let key = event.key
+    if (key == "a"){
+        let sound = document.getElementById("boom")
+        sound.play()
+    } else if (key == "s"){
+        let sound = document.getElementById("clap")
+        sound.play()
+    } else if (key == "d"){
+        let sound = document.getElementById("hihat")
+        sound.play()
+    } else if (key == "f"){
+        let sound = document.getElementById("kick")
+        sound.play()
+    } else if (key == "g"){
+        let sound = document.getElementById("openhat")
+        sound.play()
+    } else if (key == "h"){
+        let sound = document.getElementById("ride")
+        sound.play()
+    } else if (key == "j"){
+        let sound = document.getElementById("snare")
+        sound.play()
+    } else if (key == "k"){
+        let sound = document.getElementById("tink")
+        sound.play()
+    } else if (key == "l"){
+        let sound = document.getElementById("tom")
+        sound.play()
+    }
+}
+
 // Play music by mouse click
