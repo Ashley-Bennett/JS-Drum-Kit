3329ed1175b1633bb5d4adf53e3f7afbf9c07ced
Code Nation 20 <codenation@CN-20.lan>
2019-02-08 11:42:35 +0000

    Started fiddling with key presses

--- app.js
+++ app.js
@@ -1,72 +1,158 @@
 // Play drum by keyboard press
 
+
+
 let play = () => {
-    let key = event.key
-    if (key == "a"){
-        let sound = document.getElementById("boom")
-        sound.play()
-    } else if (key == "s"){
-        let sound = document.getElementById("clap")
-        sound.play()
-    } else if (key == "d"){
-        let sound = document.getElementById("hihat")
-        sound.play()
-    } else if (key == "f"){
-        let sound = document.getElementById("kick")
-        sound.play()
-    } else if (key == "g"){
-        let sound = document.getElementById("openhat")
-        sound.play()
-    } else if (key == "h"){
-        let sound = document.getElementById("ride")
-        sound.play()
-    } else if (key == "j"){
-        let sound = document.getElementById("snare")
-        sound.play()
-    } else if (key == "k"){
-        let sound = document.getElementById("tink")
-        sound.play()
-    } else if (key == "l"){
-        let sound = document.getElementById("tom")
-        sound.play()
+    let key = event.key;
+    if (key == "a") {
+      let sound = document.getElementById("boom");
+      // let shrink = document.getElementById("boom1").style;
+      
+      if (sound.paused) {
+        sound.play();
+        document.getElementById("boom1").style.transform = "scale(0.9)";
+        key.classList.remove('playing');
+
+        // document.getElementById("boom1").style.transform = "scale(1.0)";
+      } else {
+        sound.currentTime = 0;
+        key.classList.add('playing');
+
+      }
+      
+      // key.classList.add('playing');
+
+    } else if (key == "s") {
+      let sound = document.getElementById("clap");
+      if (sound.paused) {
+        sound.play();
+      } else {
+        sound.currentTime = 0;
+      }
+    } else if (key == "d") {
+      let sound = document.getElementById("hihat");
+      if (sound.paused) {
+        sound.play();
+      } else {
+        sound.currentTime = 0;
+      }
+    } else if (key == "f") {
+      let sound = document.getElementById("kick");
+      if (sound.paused) {
+        sound.play();
+      } else {
+        sound.currentTime = 0;
+      }
+    } else if (key == "g") {
+      let sound = document.getElementById("openhat");
+      if (sound.paused) {
+        sound.play();
+      } else {
+        sound.currentTime = 0;
+      }
+    } else if (key == "h") {
+      let sound = document.getElementById("ride");
+      if (sound.paused) {
+        sound.play();
+      } else {
+        sound.currentTime = 0;
+      }
+    } else if (key == "j") {
+      let sound = document.getElementById("snare");
+      if (sound.paused) {
+        sound.play();
+      } else {
+        sound.currentTime = 0;
+      }
+    } else if (key == "k") {
+      let sound = document.getElementById("tink");
+      if (sound.paused) {
+        sound.play();
+      } else {
+        sound.currentTime = 0;
+      }
+    } else if (key == "l") {
+      let sound = document.getElementById("tom");
+      if (sound.paused) {
+        sound.play();
+      } else {
+        sound.currentTime = 0;
+      }
     }
-}
-
-// Play music by mouse click
-
-let boom = () => {
-    let sound = document.getElementById("boom")
-    sound.play()
-}
-let clap = () => {
-    let sound = document.getElementById("clap")
-    sound.play()
-}
-let hihat = () => {
-    let sound = document.getElementById("hihat")
-    sound.play()
-}
-let kick = () => {
-    let sound = document.getElementById("kick")
-    sound.play()
-}
-let openhat = () => {
-    let sound = document.getElementById("openhat")
-    sound.play()
-}
-let ride = () => {
-    let sound = document.getElementById("ride")
-    sound.play()
-}
-let snare = () => {
-    let sound = document.getElementById("snare")
-    sound.play()
-}
-let tink = () => {
-    let sound = document.getElementById("tink")
-    sound.play()
-}
-let tom = () => {
-    let sound = document.getElementById("tom")
-    sound.play()
-}
+  };
+  
+  // Play music by mouse click
+  
+  let boom = () => {
+    let sound = document.getElementById("boom");
+    if (sound.paused) {
+      sound.play();
+    } else {
+      sound.currentTime = 0;
+    }
+  };
+  let clap = () => {
+    let sound = document.getElementById("clap");
+    if (sound.paused) {
+      sound.play();
+    } else {
+      sound.currentTime = 0;
+    }
+  };
+  let hihat = () => {
+    let sound = document.getElementById("hihat");
+    if (sound.paused) {
+      sound.play();
+    } else {
+      sound.currentTime = 0;
+    }
+  };
+  let kick = () => {
+    let sound = document.getElementById("kick");
+    if (sound.paused) {
+      sound.play();
+    } else {
+      sound.currentTime = 0;
+    }
+  };
+  let openhat = () => {
+    let sound = document.getElementById("openhat");
+    if (sound.paused) {
+      sound.play();
+    } else {
+      sound.currentTime = 0;
+    }
+  };
+  let ride = () => {
+    let sound = document.getElementById("ride");
+    if (sound.paused) {
+      sound.play();
+    } else {
+      sound.currentTime = 0;
+    }
+  };
+  let snare = () => {
+    let sound = document.getElementById("snare");
+    if (sound.paused) {
+      sound.play();
+    } else {
+      sound.currentTime = 0;
+    }
+  };
+  let tink = () => {
+    let sound = document.getElementById("tink");
+    if (sound.paused) {
+      sound.play();
+    } else {
+      sound.currentTime = 0;
+    }
+  };
+  let tom = () => {
+    let sound = document.getElementById("tom");
+    if (sound.paused) {
+      sound.play();
+    } else {
+      sound.currentTime = 0;
+    }
+  };
+  
