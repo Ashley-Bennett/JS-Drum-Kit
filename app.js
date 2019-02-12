d49aeb718f146ae77c5a903cd28fe2ce22260c66
Code Nation 20 <codenation@CN-20.lan>
2019-02-12 13:55:36 +0000

    1st full draft - 100% working

--- app.js
+++ app.js
@@ -6,31 +6,32 @@ let play = () => {
     let key = event.key;
     if (key == "a") {
       let sound = document.getElementById("boom");
-      // let shrink = document.getElementById("boom1").style;
-      
+      document.getElementById("boom1").classList.remove("playing")
+      void document.getElementById("boom1").offsetWidth
+      document.getElementById("boom1").classList.add("playing");
       if (sound.paused) {
         sound.play();
-        document.getElementById("boom1").style.transform = "scale(0.9)";
-        key.classList.remove('playing');
 
-        // document.getElementById("boom1").style.transform = "scale(1.0)";
       } else {
         sound.currentTime = 0;
-        key.classList.add('playing');
-
       }
-      
-      // key.classList.add('playing');
 
     } else if (key == "s") {
       let sound = document.getElementById("clap");
+      document.getElementById("clap1").classList.remove("playing")
+      void document.getElementById("clap1").offsetWidth
+      document.getElementById("clap1").classList.add("playing");
       if (sound.paused) {
         sound.play();
       } else {
         sound.currentTime = 0;
       }
+      
     } else if (key == "d") {
       let sound = document.getElementById("hihat");
+      document.getElementById("hihat1").classList.remove("playing")
+      void document.getElementById("hihat1").offsetWidth
+      document.getElementById("hihat1").classList.add("playing");
       if (sound.paused) {
         sound.play();
       } else {
@@ -38,6 +39,9 @@ let play = () => {
       }
     } else if (key == "f") {
       let sound = document.getElementById("kick");
+      document.getElementById("kick1").classList.remove("playing")
+      void document.getElementById("kick1").offsetWidth
+      document.getElementById("kick1").classList.add("playing");
       if (sound.paused) {
         sound.play();
       } else {
@@ -45,6 +49,9 @@ let play = () => {
       }
     } else if (key == "g") {
       let sound = document.getElementById("openhat");
+      document.getElementById("openhat1").classList.remove("playing")
+      void document.getElementById("openhat1").offsetWidth
+      document.getElementById("openhat1").classList.add("playing");
       if (sound.paused) {
         sound.play();
       } else {
@@ -52,6 +59,9 @@ let play = () => {
       }
     } else if (key == "h") {
       let sound = document.getElementById("ride");
+      document.getElementById("ride1").classList.remove("playing")
+      void document.getElementById("ride1").offsetWidth
+      document.getElementById("ride1").classList.add("playing");
       if (sound.paused) {
         sound.play();
       } else {
@@ -59,6 +69,9 @@ let play = () => {
       }
     } else if (key == "j") {
       let sound = document.getElementById("snare");
+      document.getElementById("snare1").classList.remove("playing")
+      void document.getElementById("snare1").offsetWidth
+      document.getElementById("snare1").classList.add("playing");
       if (sound.paused) {
         sound.play();
       } else {
@@ -66,6 +79,9 @@ let play = () => {
       }
     } else if (key == "k") {
       let sound = document.getElementById("tink");
+      document.getElementById("tink1").classList.remove("playing")
+      void document.getElementById("tink1").offsetWidth
+      document.getElementById("tink1").classList.add("playing");
       if (sound.paused) {
         sound.play();
       } else {
@@ -73,6 +89,9 @@ let play = () => {
       }
     } else if (key == "l") {
       let sound = document.getElementById("tom");
+      document.getElementById("tom1").classList.remove("playing")
+      void document.getElementById("tom1").offsetWidth
+      document.getElementById("tom1").classList.add("playing");
       if (sound.paused) {
         sound.play();
       } else {
